a39a06a3ff6d06b5e84a9cd88cbf6340e223fa00
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-21 15:08:33 -0400

    import api key

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
-url = "https://finance.yahoo.com/"
+# Top business headlines in the US right now from newsapi.org
+url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={API_KEY}"
 
 get = requests.get(url)
 content = get.text
-print(content)
